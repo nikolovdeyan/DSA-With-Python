093ce379503b535a7d2042835efd9ecb5f35cc04
Deyan Nikolov <deyan.nikolov@gmail.com>
2020-04-29 18:08:57 +0300

    Add implementations of LinkedDeque and PositionalList.

--- LinkedLists/doubly_linked.py
+++ LinkedLists/doubly_linked.py
@@ -1,3 +1,6 @@
+from empty import Empty
+
+
 class _DoublyLinkedBase:
     """A base class providing a doubly linked list representation."""
 
@@ -45,3 +48,148 @@ class _DoublyLinkedBase:
         element = node._element                         # save element
         node._prev = node._next = node._element = None  # deprecate node
         return element
+
+
+class LinkedDeque(_DoublyLinkedBase):
+    """Double-ended queue implementation based on a doubly linked list."""
+
+    def first(self):
+        """Return (but do not remove) the element at the front of the deque."""
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        return self._header._next._element  # the item after the header sentinel
+
+    def last(self):
+        """Return (but do not remove) the element at the back of the deque."""
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        return self._trailer._prev._element  # the item before the trailer sentinel
+
+    def insert_first(self, e):
+        """Add an element to the front of the deque."""
+        self._insert_between(e, self._header, self._header._next)
+
+    def insert_last(self, e):
+        """Add an element to the back of the deque."""
+        self._insert_between(e, self._trailer._prev, self._trailer)
+
+    def delete_first(self):
+        """Remove and return the element from the front of the deque.
+
+        Raise Empty exception if the deque is empty."""
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        return self._delete_node(self._header._next)
+
+    def delete_last(self):
+        """Remove and return the element from the back of the deque.
+
+        Raise Empty exception if the deque is empty."""
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        return self._delete_node(self._trailer._prev)
+
+
+class PositionalList(_DoublyLinkedBase):
+    """A sequential container of elements allowing positional access."""
+
+    class Position:
+        """An abstraction representing the location of a single element."""
+
+        def __init__(self, container, node):
+            """Constructor should not be invoked by user."""
+            self._container = container
+            self._node = node
+
+        def element(self):
+            """Return the element stored at this Position."""
+            return self._node._element
+
+        def __eq__(self, other):
+            """Return True if other is a Position representing the same location."""
+            return type(other) is type(self) and other._node is self._node
+
+        def __ne__(self, other):
+            """Return True if other does not represent the same location."""
+            return not (self == other)  # opposite of __eq__
+
+    def _validate(self, p):
+        """Return position's node, or raise appropriate error if invalid."""
+        if not isinstance(p, self.Position):
+            raise TypeError('p must be proper Position type')
+        if p._container is not self:
+            raise ValueError('p does not belong to this container')
+        if p._node._next is None:
+            raise ValueError('p is no longer valud')
+        return p._node
+
+    def _make_position(self, node):
+        """Return Position instance for given node (or None if sentinel)."""
+        if node is self._header or node is self._trailer:
+            return None
+        else:
+            return self.Position(self, node)
+
+    # ------------- accessors -------------
+    def first(self):
+        """Return the first Position in the list (or None if list is empty)."""
+        return self._make_position(self._header._next)
+
+    def last(self):
+        """Return the last Position in the list (or None if the list is empty)."""
+
+    def before(self, p):
+        """Return the Position just before Position p (or None if p is first)."""
+        node = self._validate(p)
+        return self._make_position(node._prev)
+
+    def after(self, p):
+        """Return the Position just after Position p (or None if p is last)."""
+        node = self._validate(p)
+        return self._make_position(node._next)
+
+    def __iter__(self):
+        """Generate a forward iteration of the elements of the list."""
+        cursor = self.first()
+        while cursor is not None:
+            yield cursor.element()
+            cursor = self.after(cursor)
+
+    # ------------- mutators -------------
+    # override inherited version to return Position, rather than Node
+    def _insert_between(self, e, predecessor, successor):
+        """Add element between existing nodes and return new Position."""
+        node = super()._insert_between(e, predecessor, successor)
+        return self._make_position(node)
+
+    def add_first(self, e):
+        """Insert element e at the front of the list and return new Position."""
+        return self._insert_between(e, self._header, self._header._next)
+
+    def add_last(self, e):
+        """Insert element e at the back of the list and return new Position."""
+        return self._insert_between(e, self._trailer._prev, self._trailer)
+
+    def add_before(self, p, e):
+        """Insert element e into the list before Position p and return new Position."""
+        original = self._validate(p)
+        return self._insert_between(e, original._prev, original)
+
+    def add_after(self, p, e):
+        """Insert element e into the list after Position p and return new Position."""
+        original = self._validate(p)
+        return self._insert_between(e, original, original._next)
+
+    def delete(self, p):
+        """Remove and return the element at Position p."""
+        original = self._validate(p)
+        return self._delete_node(original)  # the inherited method returns the element
+
+    def replace(self, p, e):
+        """Replace the element at Position p with e.
+
+        Return the element formerly at Position p."""
+        original = self._validate(p)
+        old_value = original._element  # save to return
+        original._element = e
+        return old_value
