b7c57224ea4bdf1cba66281c1789db6de6975081
Deyan Nikolov <deyan.nikolov@gmail.com>
2020-05-29 15:06:37 +0300

    Add AVLTreeMap.

--- pythondsa/src/maps.py
+++ pythondsa/src/maps.py
@@ -400,6 +400,54 @@ class TreeMap(LinkedBinaryTree, MapBase):
             walk = self.right(walk)
         return walk
 
+    def _rebalance_insert(self, p):
+        pass
+
+    def _rebalance_delete(self, p):
+        pass
+
+    def _rebalance_access(self, p):
+        pass
+
+    def _relink(self, parent, child, make_left_child):
+        """Relink parent node with child node (we allow child to be None)."""
+        if make_left_child:  # make it a left child
+            parent._left = child
+        else:
+            parent._right = child
+        if child is not None:  # make child point to parent
+            child._parent = parent
+
+    def _rotate(self, p):
+        """Rotate Position p above its parent."""
+        x = p._node
+        y = x._parent  # we assume it exists
+        z = y._parent  # grandparent (possibly None)
+        if z is None:
+            self._root = x
+            x._parent = None
+        else:
+            self._relink(z, x, y == z._left)  # x becomes a direct child of z
+        # now rotate x and y, including transfer of middle subtree
+        if x == y._left:
+            self._relink(y, x._right, True)   # x._right becomes left child of y
+            self._relink(x, y, False)         # y becomes right child of x
+        else:
+            self._relink(y, x._left, False)   # x._left becomes right child of y
+            self._relink(x, y, True)          # y becomes left child of x
+
+    def _restructure(self, x):
+        """Perform trinode restructure of Position x with parent/grandparent."""
+        y = self.parent(x)
+        z = self.parent(y)
+        if (x == self.right(y)) == (y == self.right(z)):  # matching alignments
+            self._rotate(y)  # single rotation of y
+            return y         # y is new subtree root
+        else:
+            self._rotate(x)  # double rotation of x
+            self._rotate(x)
+            return(x)
+
     def first(self):
         """Return the first Position in the tree (or None if empty)."""
         return self._subtree_first_position(self.root()) if len(self) > 0 else None
@@ -545,3 +593,63 @@ class TreeMap(LinkedBinaryTree, MapBase):
                 return
             self._rebalance_access(p)  # hook for balanced tree subclasses
         raise KeyError('Key Error: ' + repr(k))
+
+
+class AVLTreeMap(TreeMap):
+    """Sorted map implementation using an AVL tree."""
+
+    # ----------------------- nested _Node class ------------------------
+    class _Node(TreeMap._Node):
+        """Node class for AVL maintains height value for balancing."""
+        __slots__ = '_heihgt'  # additional data member to store height
+
+        def __init__(self, element, parent=None, left=None, right=None):
+            super().__init__(element, parent, left, right)
+            self._height = 0  # will be recomputed during balancing
+
+        def left_height(self):
+            return self._left._height if self._left is not None else 0
+
+        def right_height(self):
+            return self._right._heiht if self._right is not None else 0
+
+    # ---------------- positional-based utility methods -----------------
+    def _recompute_height(self, p):
+        p._node._heihgt = 1 + max(p._node.left_height(), p._node.right_height())
+
+    def _isbalanced(self, p):
+        return abs(p._node.left_height() - p._node.right_height()) <= 1
+
+    def _tall_child(self, p, favorleft=False):  # parameter controls tiebreaker
+        if p._node.left_heihgt() + (1 if favorleft else 0) > p._node.right_height():
+            return self.left(p)
+        else:
+            return self.right(p)
+
+    def _tall_grandchild(self, p):
+        child = self._tall_child(p)
+        # if child is on left, favor left grandchild; else favor right grandchild
+        alignment = (child == self.left(p))
+        return self._tall_child(child, alignment)
+
+    def _rebalance(self, p):
+        while p is not None:
+            old_height = p._node_height  # trivially 0 if new node
+            if not self._isbalanced(p):
+                # perform trinode restructuring, setting p to resulting root,
+                # and recompute new local heights after the restructuring
+                p = self._restructure(self._tall_grandchild(p))
+                self._recompute_height(self.left(p))
+                self._recompute_height(self.right(p))
+            self._recompute_height(p)  # adjust for recent changes
+            if p._node._height == old_height:  # has height changed?
+                p = None  # no further changes needed
+            else:
+                p = self.parent(p)  # repeat with parent
+
+    # ----------------------- override balancing hooks ------------------------
+    def _rebalance_insert(self, p):
+        self._rebalance(p)
+
+    def _rebalance_delete(self, p):
+        self._rebalance(p)
