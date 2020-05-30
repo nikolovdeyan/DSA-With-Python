70a32616e589450c7d0e1463da0599a34a224741
Deyan Nikolov <deyan.nikolov@gmail.com>
2020-05-30 22:41:44 +0300

    Add SplayTreeMap

--- pythondsa/src/maps.py
+++ pythondsa/src/maps.py
@@ -653,3 +653,33 @@ class AVLTreeMap(TreeMap):
 
     def _rebalance_delete(self, p):
         self._rebalance(p)
+
+
+class SplayTreeMap(TreeMap):
+    """Sorted map implementation using a splay tree."""
+    # ---------------------------- splay operation-----------------------------
+    def _splay(self, p):
+        while p != self.root():
+            parent = self.parent(p)
+            grand = self.parent(parent)
+            if grand is None:
+                # zig case
+                self._rotate(p)
+            elif (parent == self.left(grand)) == (p == self.left(parent)):
+                # zig-zig case
+                self._rotate(parent)  # move PARENT up
+                self._rotate(p)       # then move p up
+            else:
+                # zig-zag case
+                self._rotate(p)       # move p up
+                self._rotate(p)       # move p up again
+
+    # ----------------------- override balancing hooks ------------------------
+    def _rebalance_insert(self, p):
+        self._splay(p)
+
+    def _rebalance_delete(self, p):
+        self._splay(p)
+
+    def _rebalance_access(self, p):
+        self._splay(p)
